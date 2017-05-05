9a40834e08c8b23d15ec9972c6017482c48b88ce
tedisbed <tbrombac@nd.edu>
2017-05-05 15:17:15 -0400

    done with main part of game space

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
 !#/usr/bin/env python3
+
+import pygame
+import math
+
+class GameSpace:
+    def main(self):
+        pygame.init()
+        self.size = self.width,self.height = 640,480
+        self.black = 0,0,0
+        self.screen = pygame.display.se_mode(self.siz)
+
+        while_loop = 1
+        self.clock = pygame.time.Clock()
+
+        pygame.key.set_repeat(1,1)
+        while while_loop:
+            self.clock.tick(60)
+            for even in pygame.event.get()
+                if event.type == pygame.QUIT:
+                    while_loop =0
+                    break
+
+            self.screen.fill(self.black)
+
+            pygame.display.flip()
+
+if __name__ == "__main__":
+    gs = GameSpace()
+    gs.main()
